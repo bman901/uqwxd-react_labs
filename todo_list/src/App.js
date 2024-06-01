b9700e08d4db8b0e07c924dc0bbc19e099fb03c8
Ben Kehoe <b_man901@hotmail.com>
2024-06-01 13:46:20 +0100

    Toggle complete function

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -28,6 +28,15 @@ const App = () => {
   }
 
   // Add the toggleComplete code here
+  function toggleComplete(id) {
+    let updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
 
   // Add the submitEdits code here
 
@@ -40,7 +49,15 @@ const App = () => {
       </form>
       {todos.map((todo) => (
         <div className="todo" key={todo.id}>
-          <div className="todo-text">{todo.text}</div>
+          <div className="todo-text">
+            <input
+              type="checkbox"
+              id="completed"
+              checked={todo.completed}
+              onChange={() => toggleComplete(todo.id)}
+            />
+            {todo.text}
+          </div>
           <button onClick={() => deleteTodo(todo.id)}>Delete</button>
         </div>
       ))}
